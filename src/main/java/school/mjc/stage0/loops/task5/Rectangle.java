247c9b5c4c8b2832f152d81b38e7d3054efe02a6
Stanislau_Sukora <ssukora@estee.com>
2023-07-21 10:43:47 +0300

    Tough one

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        for (int i = 0; i < height; i++) {
+            String line = "";
+            for (int j = 0; j < length; j++) {
+                if(i == 0 || j == 0 || (length - j - 1) == 0 || (height - i - 1) == 0) {
+                    line = line + "8";
+                } else {
+                    line = line + " ";
+                }
+            }
+            System.out.println(line);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
-
+    public void printSquareFrom8s(int sideLength) {
+        for (int i = 0; i < sideLength; i++) {
+            String line = "";
+            for (int j = 0; j < sideLength; j++) {
+                if(i == 0 || j == 0 || (sideLength - j - 1) == 0 || (sideLength - i - 1) == 0) {
+                    line = line + "8";
+                } else {
+                    line = line + " ";
+                }
+            }
+            System.out.println(line);
+        }
     }
 }
